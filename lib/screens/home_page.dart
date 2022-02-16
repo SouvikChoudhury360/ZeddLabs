fd8a6ad3c90a2e405ca114a7338f9cf4a78fe14d
SouvikChoudhury360 <choudhurysouvik2003@gmail.com>
2022-02-16 11:18:30 +0530

    UI changed

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -8,8 +8,8 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-final cardData = <String>['card 1', 'card 2', 'card 3'];
-var cardDataChangeable = <String>['card 1', 'card 2', 'card 3'];
+final cardData = <String>['Card 1', 'Card 2', 'Card 3'];
+var cardDataChangeable = <String>['Card 1', 'Card 2', 'Card 3'];
 
 class _HomePageState extends State<HomePage> {
   @override
@@ -17,9 +17,14 @@ class _HomePageState extends State<HomePage> {
     const title = 'Zedd Labs Assignment';
     return Scaffold(
         appBar: AppBar(
-          title: const Text(title),
+          backgroundColor: const Color(0xFF150050),
+          title: Center(child: const Text(title, style: TextStyle(color: Color(0xFFFEFBF3)),)),
         ),
-        body: ListView(children: cardDataChangeable.map(_buildItem).toList()));
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: Color(0xFF3F0071),
+          child: ListView(children: cardDataChangeable.map(_buildItem).toList())));
   }
 
   Widget _buildItem(String data) {
@@ -46,13 +51,41 @@ class _HomePageState extends State<HomePage> {
           }
         },
         child: Container(
-          height: 80.0,
-          padding: const EdgeInsets.all(16.0),
-          decoration: const BoxDecoration(
-            border: Border(bottom: BorderSide()),
+          width: MediaQuery.of(context).size.width - 32,
+          height: 160.0,
+          margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          decoration: BoxDecoration(
+            color: Color(0x4D000000),
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(color: Color(0xCC3F0071)),
           ),
           child: Center(
-            child: Text(data),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(data, style: TextStyle(color: Color(0xFFFEFBF3), fontSize: 36,),),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        const Icon(Icons.arrow_upward_sharp, color: Colors.green),
+                        Text('Swipe Left', style: TextStyle(color: Color(0xFFFEFBF3), fontSize: 15,),),
+                      ],
+                    ),
+                    Column(
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        const Icon(Icons.close, color: Colors.red,),
+                        SizedBox(height: 8.0),
+                        Text('Swipe Right', style: TextStyle(color: Color(0xFFFEFBF3), fontSize: 15,),),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
